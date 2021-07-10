2fb91084b95776ffbb72912503cbde59e48047bd
lekshmisiv <lekshmisiv@gmail.com>
2021-07-10 08:14:18 +0530

    initial commit

---
+++ adminpannel/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+     path('login', views.loginadmin, name='login'),
+     path('', RedirectView.as_view(url='login')),
+     path('adminlogout', views.logoutadmin, name='adminlogout'),
+     path('dashboard', views.admindashboard, name='admindashboard'),
+     ]
---
+++ adminpannel/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse, reverse_lazy
+
+
+def loginadmin(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('admindashboard') )
+    else:	
+        if request.method == 'POST':
+            login_form = LoginForm(request.POST)
+            if login_form.is_valid():
+                username = login_form.cleaned_data['username']
+                password = login_form.cleaned_data['password']
+
+                user = authenticate(username=username, password=password)
+                                
+                if user is not None:
+                    if user.is_active and user.is_superuser:
+                        login(request,user)	
+                        return HttpResponseRedirect(reverse('admindashboard') )
+                    else:
+                        return HttpResponse('Your account is not active')
+                else:
+                    return HttpResponse('The Account does not exists')
+            else:
+                login_form = LoginForm()
+                return render(request, "login.html",{"form":login_form})
+        else:
+            login_form = LoginForm()
+        return render(request,'login.html',{"form":login_form})
+     
+def checksuperuser(user):
+    return user.is_superuser   
+
+@user_passes_test(checksuperuser,login_url = reverse_lazy('login'))
+def logoutadmin(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))  
+
+@user_passes_test(checksuperuser,login_url = reverse_lazy('login'))
+def admindashboard(request):
+    return render(request,'adminpannel/admindashboard.html',{})   
